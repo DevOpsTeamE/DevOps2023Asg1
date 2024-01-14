c335b9005f614675e4a11a74e801c938c7c66821
xinenlim <S10221987@connect.np.edu.sg>
2024-01-14 23:29:15 +0800

    added login route

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from controllers.home_controller import HomeController
 def create_app():
     app =Flask(__name__)
 
+    @app.route("/login")
+    def loginPage():
+        return "Login Page"
+
+
     app.register_blueprint(HomeController)
     return app
