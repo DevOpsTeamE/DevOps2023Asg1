9f9a4f9310f5696d32c380f101ce156109b98f1b
joshuang321 <joshuang21978@gmail.com>
2024-01-27 23:53:52 +0800

    Fixing setup

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ sudo rm -rf DevOps2023Asg1
 
 cd devops
 
-killall screen
+sudo killall screen
+sudo pkill gunicorn
 sudo apt update
 sudo apt --assume-yes install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
 sudo apt --assume-yes install python3-venv
