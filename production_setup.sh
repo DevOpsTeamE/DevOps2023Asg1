95f90d1d1fd85970052096979c03ed6189ba473f
joshuang321 <joshuang21978@gmail.com>
2024-01-28 20:42:05 +0800

    Tweaked release.yaml and added production setup script

---
+++ production_setup.sh
@@ -0,0 +1,29 @@
+sudo rm -rf *
+
+sudo pkill python3
+sudo apt update
+sudo apt --assume-yes install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+sudo apt --assume-yes install python3-venv
+sudo apt update
+sudo apt --assume-yes install mysql-server
+sudo systemctl start mysql.service
+sudo systemctl status mysql.service
+
+sudo mysql --user=root --password=root < predatabase_setup.sql
+sudo mysql --user=root --password=root < database_setup.sql
+
+sudo apt --assume-yes install nginx
+sudo mv -f nginx.config /etc/nginx/sites-available/devops
+sudo ln -sf /etc/nginx/sites-available/devops /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+
+python3 -m venv DevOpsEnv
+source DevOpsEnv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install Flask
+python3 -m pip install mysql-connector-python
+python3 -m pip install wheel
+python3 -m pip install gunicorn
+sudo ufw allow 5000
+sudo apt update
+python3 -m gunicorn --daemon -w 4 'main:create_app()'
